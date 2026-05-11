265cdf032865dc6f757baa9474dad2e109eca409
Kanghyun A. Kim <257200527+kak06-thebldgs@users.noreply.github.com>
2026-05-11 21:02:06 +0900

    chore: update Homebrew formula to v0.5.8

--- Formula/thask.rb
+++ Formula/thask.rb
@@ -3,26 +3,26 @@
 class Thask < Formula
   desc "Dependency visualization CLI for AI-assisted development"
   homepage "https://github.com/kimgh06/Thask"
-  version "0.5.7"
+  version "0.5.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kimgh06/Thask/releases/download/v#{version}/thask-darwin-arm64.tar.gz"
-      sha256 "3ffe557cfce56893a517736004c550abe00cc604ab58f865ff38ba4770e81db0"
+      sha256 "2bf085e6861fd8e978c2a0caa6dbf70eb9d0d138618a5c1a2fb4c494c7a5e05e"
     else
       url "https://github.com/kimgh06/Thask/releases/download/v#{version}/thask-darwin-amd64.tar.gz"
-      sha256 "84e14d295f0007e421956a1cbc3a16b8396f42dad14e69452ed83903c0a5bc00"
+      sha256 "ed19e3f112c5267b728e0d25f9badc6c5b9ecc30dd0a192ee9c658d569e08f34"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kimgh06/Thask/releases/download/v#{version}/thask-linux-arm64.tar.gz"
-      sha256 "fa20d74d246a5a706e92d18b119ba23a6afe586e496d54fe9f80f044a4f5f2a7"
+      sha256 "3590521d5824a9f527e0602f4a8f31390f5461e17246be36ae3494afe86a364a"
     else
       url "https://github.com/kimgh06/Thask/releases/download/v#{version}/thask-linux-amd64.tar.gz"
-      sha256 "a4f06652c97b25c0e55ae5d3d7d350b462e7a1266c54dabd7b7eab8e05dd483c"
+      sha256 "bbf2a811e33b8f3d05cdd7739f8c1004686a67073c769757dc981c5e58713238"
     end
   end
 
